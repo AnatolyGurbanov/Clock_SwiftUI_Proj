a06184c0784b29a5e952e56b57f19838955e8adb
Anatoly Gurbanov <tomgurbanov@gmail.com>
2022-04-07 00:18:06 +0600

    Create arc shape

---
+++ Clock/Clock/Shapes/Arc.swift
@@ -0,0 +1,36 @@
+//
+//  Arc.swift
+//  Clock
+//
+//  Created by Anatoly Gurbanov on 07.04.2022.
+//
+
+import SwiftUI
+
+// MARK: - Arc shape
+
+struct Arc: Shape {
+
+    // Variable
+
+    var startAngle: Angle = .radians(0)
+    var endAngle: Angle = .radians(Double.pi * 2)
+    var clockWise: Bool = true
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width/2, rect.height/2)
+        
+        path.addArc(
+            center: center,
+            radius: radius,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: clockWise
+        )
+
+        return path
+    }
+}
