a0a5ea42f9e656bf9d6e7f0673937d27d8b8a475
Anatoly Gurbanov <tomgurbanov@gmail.com>
2022-04-06 23:58:29 +0600

    Create new project

---
+++ Clock/Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Anatoly Gurbanov on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
