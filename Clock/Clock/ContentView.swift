8b566fa9d3cb56b8d719faba9ed39ef5c1f28a14
Anatoly Gurbanov <tomgurbanov@gmail.com>
2022-04-08 00:58:38 +0600

    Make clock working

--- Clock/Clock/ContentView.swift
+++ Clock/Clock/ContentView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var date: Date = Date()
+    
     var body: some View {
-        ZStack {
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.hour, .minute, .second], from: date)
+        
+        /// convert date to angle
+        var hourAngle: Double = 0
+        var minuteAngle: Double = 0
+        var secondAngle: Double = 0
+        
+        if let hour = dateComponents.hour,
+           let minute = dateComponents.minute,
+           let second = dateComponents.second {
+            
+            let radianInOneHour = 2 * Double.pi / 12
+            let radianInOneMinute = 2 * Double.pi / 60
+            
+            minuteAngle = Double(minute) * radianInOneMinute
+            
+            let actualHour = Double(hour) + (Double(minute)/60)
+            
+            hourAngle = actualHour * radianInOneHour
+            
+            secondAngle = Double(second) * radianInOneMinute
+        }
+        
+        return ZStack {
             Arc(
                 startAngle: .radians(0),
                 endAngle: .radians(Double.pi * 2),
@@ -22,22 +48,32 @@ struct ContentView: View {
                 .fill()
                 .frame(width: 15, height: 15, alignment: .center)
             /// Часовая стрелка
-            Hand(offset: 10)
+            Hand(offset: 40)
                 .fill()
                 .frame(width: 4, alignment: .center)
+                .rotationEffect(.radians(hourAngle))
             /// Минутная стрелка
             Hand(offset: 10)
                 .fill()
                 .frame(width: 3, alignment: .center)
+                .rotationEffect(.radians(minuteAngle))
             /// Секундная стрелка
             Hand(offset: 5)
                 .foregroundColor(.red)
                 .frame(width: 2, alignment: .center)
+                .rotationEffect(.radians(secondAngle))
             Circle()
                 .foregroundColor(.red)
                 .frame(width: 7, height: 7, alignment: .center)
         }
         .frame(width: 200, height: 200, alignment: .center)
+        .onAppear(perform: start)
+    }
+    
+    func start() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.date = Date()
+        }
     }
 }
 
